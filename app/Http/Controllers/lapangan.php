c4b610b7ad94ab80cd1b7ac4ec908e0cc0ce3578
ahmad al fajri <116804256+inven22@users.noreply.github.com>
2023-12-13 11:01:20 +0700

    Update lapangan.php

--- app/Http/Controllers/lapangan.php
+++ app/Http/Controllers/lapangan.php
@@ -3,114 +3,90 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use DB;
+use App\Models\Lapang;
 
 class lapangan extends Controller
 {
     public function index()
     {
-    	$la = DB::table('lapangans')->get();
-        return view('lapangan.index',compact('la'));
-    } 
-
-    public function Add()
-    {
-    $all = DB::table('penyewa')->get();
-    return view('lapangan.add',compact('all'));
+        $la = Lapang::all();
+        return view('lapangan.index', compact('la'));
     }
-    
+
+    public function add()
+    {
+        // Di sini, jika kamu ingin menampilkan data penyewa dari model Penyewa, kamu dapat melakukannya.
+        // $all = Penyewa::all();
+
+        return view('lapangan.add');
+    }
+
     public function insert(Request $request)
     {
         $request->validate([
             'nama' => 'required',
             'deskripsi' => 'required',
             'waktu' => 'required',
-            'file_image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // Validation rule for images
-        
+            'file_image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
-    
-      // Handle image file upload
-      $imageFile = $request->file('file_image');
-      $nama_image_file = time() . '_' . $imageFile->getClientOriginalName();
-      $tujuan_upload_image = 'storage/images';
-      $imageFile->move($tujuan_upload_image, $nama_image_file);
 
-        DB::table('lapangans')->insert([
+        $imageFile = $request->file('file_image');
+        $nama_image_file = time() . '_' . $imageFile->getClientOriginalName();
+        $tujuan_upload_image = 'storage/images';
+        $imageFile->move($tujuan_upload_image, $nama_image_file);
+
+        Lapang::create([
             'nama' => $request->nama,
             'deskripsi' => $request->deskripsi,
             'waktu' => $request->waktu,
             'file_image' => $request->file_image,
             'file_path_image' => $tujuan_upload_image . '/' . $nama_image_file,
-    
         ]);
-    
-        return Redirect()->route('la.index')->with('success','Add data successfully!');    
+
+        return redirect()->route('la.index')->with('success', 'Data berhasil ditambahkan!');
     }
 
-    public function Edit($id)
+    public function edit($id)
     {
-    $edit=DB::table('lapangans')
-         ->where('id',$id)
-         ->first();
-    return view('lapangan.edit', compact('edit'));     
+        $edit = Lapang::findOrFail($id);
+        return view('lapangan.edit', compact('edit'));
     }
-    
-    public function Update(Request $request,$id)
-    {
-         // Handle image file upload
-      $imageFile = $request->file('file_image');
-      $nama_image_file = time() . '_' . $imageFile->getClientOriginalName();
-      $tujuan_upload_image = 'storage/images';
-      $imageFile->move($tujuan_upload_image, $nama_image_file);
-      
-        DB::table('lapangans')->where('id', $id)->first();        
-        $data = array();
-        $data['nama'] = $request->nama;
-        $data['deskripsi'] = $request->deskripsi;  
-        $data['waktu'] = $request->waktu;  
-        $data['file_image'] = $request->file_image;
-        $data['file_path_image'] = $tujuan_upload_image . '/' . $nama_image_file;
-      
-        $update = DB::table('lapangans')->where('id', $id)->update($data);
-    
-        if ($update) 
-    {
-            
-            return Redirect()->route('la.index')->with('success','Updated successfully!');                     
-    }
-        else
-    {
-        $notification=array
-        (
-        'messege'=>'error ',
-        'alert-type'=>'error'
-        );
-        return Redirect()->route('la.index')->with($notification);
-    }
-     
+    public function update(Request $request, $id)
+{
+    $request->validate([
+        'nama' => 'required',
+        'deskripsi' => 'required',
+        'waktu' => 'required',
+        'file_image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+    ]);
+
+    $lapangan = Lapang::findOrFail($id);
+
+    if ($request->hasFile('file_image')) {
+        $imageFile = $request->file('file_image');
+        $nama_image_file = time() . '_' . $imageFile->getClientOriginalName();
+        $tujuan_upload_image = 'storage/images';
+        $imageFile->move($tujuan_upload_image, $nama_image_file);
+
+        $lapangan->file_image = $request->file_image;
+        $lapangan->file_path_image = $tujuan_upload_image . '/' . $nama_image_file;
     }
 
-    public function Delete($id)
-    {
+    $lapangan->nama = $request->nama;
+    $lapangan->deskripsi = $request->deskripsi;
+    $lapangan->waktu = $request->waktu;
+    $lapangan->save();
+
+    return redirect()->route('la.index')->with('success', 'Data berhasil diperbarui!');
+}
+public function delete($id)
+{
+    $lapangan = Lapang::findOrFail($id);
     
-        $delete = DB::table('lapangans')->where('id', $id)->delete();
-        if ($delete)
-                            {
-                            $notification=array(
-                            'messege'=>'Successfully  Deleted ',
-                            'alert-type'=>'success'
-                            );
-                            return Redirect()->back()->with($notification);                      
-                            }
-             else
-                  {
-                  $notification=array(
-                  'messege'=>'error ',
-                  'alert-type'=>'error'
-                  );
-                  return Redirect()->back()->with($notification);
-    
-                  }
-    
-      }
+    if($lapangan->delete()) {
+        return redirect()->route('la.index')->with('success', 'Data berhasil dihapus!');
+    } else {
+        return redirect()->route('la.index')->with('error', 'Gagal menghapus data!');
+    }
+}
 }
